d345ec60dfef16b64a91b403e3f847a8afcccc48
jorneytoplay@gmail.com <jorneytoplay@gmail.com>
2021-10-26 20:24:59 +0300

    Edit Person class

--- src/main/java/com/example/dbdemo/DAO/PersonDAO.java
+++ src/main/java/com/example/dbdemo/DAO/PersonDAO.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Lombok;
 
 import static com.example.dbdemo.Constants.PersonConstants.PersonSQL.*;
 
--- src/main/java/com/example/dbdemo/Entity/Person.java
+++ src/main/java/com/example/dbdemo/Entity/Person.java
@@ -1,11 +1,17 @@
 package com.example.dbdemo.Entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Person {
 
+    @Id
     int id = 0;
     String nickname;
     int age = 0;
